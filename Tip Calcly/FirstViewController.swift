8d68a84396abd397dc8b13123d25e99970e436bf
Abdalrahim Abdulla <abdalrahim.abdullah1@gmail.com>
2016-07-12 11:09:18 +0800

    Basic Setup

---
+++ Tip Calcly/FirstViewController.swift
@@ -0,0 +1,52 @@
+//
+//  FirstViewController.swift
+//  Tip Calcly
+//
+//  Created by Abdalrahim Abdullah on 11/7/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    @IBOutlet var billAmountField: UITextField!
+    @IBOutlet weak var tipSelector: UITextField!
+    @IBOutlet var tipAmountField: UITextField!
+    @IBOutlet var totalAmountField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        guard let billAmount = Double(billAmountField.text!) else {
+            //show error
+            billAmountField.text = ""
+            tipAmountField.text = ""
+            totalAmountField.text = ""
+            return
+        }
+        
+        var tipPercentage = Double.self
+        
+        
+        
+        let roundedBillAmount = round(100*billAmount)/100
+        let tipAmount = roundedBillAmount * Double(tipSelector.text!)!
+        let roundedTipAmount = round(100*tipAmount)/100
+        let totalAmount = roundedBillAmount + roundedTipAmount
+        
+        if (!billAmountField.editing) {
+            billAmountField.text = String(format: "%.2f", roundedBillAmount)
+        }
+        tipAmountField.text = String(format: "%.2f", roundedTipAmount)
+        totalAmountField.text = String(format: "%.2f", totalAmount)
+    }
+}
+
---
+++ Tip Calcly/ListResultsTableView.swift
@@ -0,0 +1,9 @@
+//
+//  ListResultsTableView.swift
+//  Tip Calcly
+//
+//  Created by Abdalrahim Abdullah on 12/7/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import Foundation
