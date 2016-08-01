61be3d4f0effae77db0269c3bfe720090f3abdfb
Abdalrahim Abdulla <abdalrahim.abdullah1@gmail.com>
2016-08-01 11:32:12 +0800

    Tip Calcly 1.0

--- Tip Calcly/SecondViewController.swift
+++ Tip Calcly/SecondViewController.swift
@@ -44,41 +44,10 @@ class SecondViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
         
         tableView.dataSource = self
         
+        IHKeyboardAvoiding.setAvoidingView(tableView)
+        
         self.addDoneButtonOnKeyboard()
         
-    }
-    
-    override func viewDidDisappear(animated: Bool) {
-        super.viewDidDisappear(animated)
-    }
-    
-    func subscribeToKeyboardNotifications(){
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
-    }
-    
-    func unsubscribeFromKeyboardNotifications() {
-        NSNotificationCenter.defaultCenter().removeObserver(self, name:
-            UIKeyboardWillShowNotification, object: nil)
-        NSNotificationCenter.defaultCenter().removeObserver(self, name:
-            UIKeyboardWillHideNotification, object: nil)
-    }
-    
-    func keyboardWillShow(notification: NSNotification){
-        if keyboardIsDisplayed == false {
-            keyboardIsDisplayed = true
-        }
-    }
-    
-    func keyboardWillHide(notification: NSNotification){
-        if keyboardIsDisplayed == true {
-            keyboardIsDisplayed = false
-        }
-    }
-    
-    func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
-        view.endEditing(true)
     }
     
     func calculateResults() {
